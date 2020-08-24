6ec81cbee9e3f7322f9b04b4c764e6f484202dc2
Arnab  Das <imarnab01@gmail.com>
2020-08-24 23:50:58 +0530

    signup

--- src/App/Components/Auth/index.js
+++ src/App/Components/Auth/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import SignupPhone from "./signupPhone";
+import SignupMail from "./signup";
 export class AuthHome extends Component {
   render() {
     return (
       <div>
         <SignupPhone />
+        <SignupMail></SignupMail>
       </div>
     );
   }
