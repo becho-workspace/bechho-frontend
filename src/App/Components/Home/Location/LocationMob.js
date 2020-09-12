97582272aa0695322d3ad13c1fd433930dac71c4
Arnab  Das <imarnab01@gmail.com>
2020-09-12 14:07:43 +0530

    Mobile location solved

--- src/App/Components/Home/Location/LocationMob.js
+++ src/App/Components/Home/Location/LocationMob.js
@@ -1,22 +1,64 @@
 import React, { Component } from "react";
-import { MapPin } from "react-feather";
+import Delhi from "../../../Assets/Images/Home/Delhi.png";
+import Noida from "../../../Assets/Images/Home/Noida.png";
+import Gurgaon from "../../../Assets/Images/Home/Gurgaon.png";
+import Bangalore from "../../../Assets/Images/Home/Bangalore.png";
+import Pune from "../../../Assets/Images/Home/Pune.png";
+import Kolkata from "../../../Assets/Images/Home/Kolkata.png";
+
 class LocationMob extends Component {
+  state = {
+    width: window.innerWidth,
+  };
+
   render() {
     return (
       <div>
-        <div className="mr-3 ml-3">
-          <div className="th-location-mob-container d-flex justify-content-between">
-            <div className="d-flex align-items-center">
-              <MapPin size={20} color="#332a7c" />
-              <span className="th-mob-location-title ml-1">
-                Preferred Location
-              </span>
+        {this.state.width < 768 ? (
+          <div className="">
+            <h4 className="text-center mb-3 th-mob-location-title">
+              Preferred Location
+            </h4>
+            <div>
+              <div className="row">
+                <div className="col-6 text-center" onClick={this.props.clicked}>
+                  <img src={Delhi} alt="" className="th-location-mob-circle" />
+                  <p className="mt-2 th-mob-location-city">Delhi</p>
+                </div>
+                <div className="col-6 text-center" onClick={this.props.clicked}>
+                  <img src={Noida} alt="" className="th-location-mob-circle" />
+                  <p className="mt-2 th-mob-location-city">Noida</p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-6 text-center" onClick={this.props.clicked}>
+                  <img
+                    src={Gurgaon}
+                    alt=""
+                    className="th-location-mob-circle"
+                  />
+                  <p className="mt-2 th-mob-location-city">Gurugram</p>
+                </div>
+                <div className="col-6 text-center" onClick={this.props.clicked}>
+                  <img
+                    src={Bangalore}
+                    alt=""
+                    className="th-location-mob-circle"
+                  />
+                  <p className="mt-2 th-mob-location-city">Bangalore</p>
+                </div>
+              </div>
             </div>
-            <div className="d-flex align-items-center">
-              <span className="th-mob-location-city ">Set City</span>
+            {/* <div className="col-4 text-center">
+              <img src={Pune} alt="" className="th-location-mob-circle" />
+              <p className="mt-2 th-mob-location-city ">Pune</p>
             </div>
+            <div className="col-4 text-center">
+              <img src={Kolkata} alt="" className="th-location-mob-circle" />
+              <p className="mt-2 th-mob-location-city ">Kolkata</p>
+            </div> */}
           </div>
-        </div>
+        ) : null}
       </div>
     );
   }
--- src/App/Layouts/Header/headerMob.js
+++ src/App/Layouts/Header/headerMob.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import Modal from "react-bootstrap/Modal";
 import Logo from "../../Assets/Images/Header/Logo.png";
 import { Menu, User, MapPin } from "react-feather";
 import SideDrawer from "../Sidedrawer/Sidedrawer";
 import BackDrop from "../Sidedrawer/Backdrop";
+import Location from "../../Components/Home/Location/LocationMob";
 
 class HeaderMobile extends Component {
   constructor(props) {
     super(props);
     this.state = {
       sideDrawerOpen: false,
+      show_modal: false,
     };
   }
 
@@ -26,6 +29,18 @@ class HeaderMobile extends Component {
     this.setState({ sideDrawerOpen: false });
   };
 
+  handleShowModal = () => {
+    this.setState({
+      show_modal: !this.state.show_modal,
+    });
+  };
+
+  handleCloseModal = () => {
+    this.setState({
+      show_modal: !this.state.show_modal,
+    });
+  };
+
   render() {
     let backdrop;
     if (this.state.sideDrawerOpen) {
@@ -33,24 +48,41 @@ class HeaderMobile extends Component {
     }
 
     return (
-      <div className="pr-3 pl-3 pt-2 pb-2 shadow-sm th-mob-header">
-        <div className="d-flex justify-content-between align-items-center">
-          <Menu
-            size={28}
-            className="mr-4"
-            onClick={this.drawerToggleClickHandler}
-          />
-          <img src={Logo} alt="logo" style={{ width: "100px" }} />
-          <div>
-            <MapPin size={24} className="mr-3" />
-            {/* <User size={24} className="mr-3" /> */}
+      <div>
+        <div className="pr-3 pl-3 pt-2 pb-2 shadow-sm th-mob-header">
+          <div className="d-flex justify-content-between align-items-center">
+            <Menu
+              size={28}
+              className="mr-4"
+              onClick={this.drawerToggleClickHandler}
+            />
+            <img src={Logo} alt="logo" style={{ width: "100px" }} />
+            <div>
+              <MapPin
+                size={24}
+                className="mr-3"
+                onClick={this.handleShowModal}
+              />
+              {/* <User size={24} className="mr-3" /> */}
+            </div>
           </div>
+          <SideDrawer
+            show={this.state.sideDrawerOpen}
+            clicked={this.drawerToggleClickHandler}
+          />
+          {backdrop}
         </div>
-        <SideDrawer
-          show={this.state.sideDrawerOpen}
-          clicked={this.drawerToggleClickHandler}
-        />
-        {backdrop}
+        <Modal
+          size="md"
+          show={this.state.show_modal}
+          centered
+          onHide={this.handleCloseModal}
+          aria-labelledby="example-modal-sizes-title-sm"
+        >
+          <Modal.Body>
+            <Location clicked={this.handleCloseModal} />
+          </Modal.Body>
+        </Modal>
       </div>
     );
   }
