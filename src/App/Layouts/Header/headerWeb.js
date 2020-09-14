dbff6afde082388f8eb4a63acfe59c79c918fe5d
Arnab  Das <imarnab01@gmail.com>
2020-09-15 01:21:36 +0530

    logout added to header

--- src/App/Layouts/Header/headerWeb.js
+++ src/App/Layouts/Header/headerWeb.js
@@ -6,8 +6,12 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import InputGroup from "react-bootstrap/InputGroup";
 import Logo from "../../Assets/Images/Header/Logo.png";
-import { MapPin, User, Tv } from "react-feather";
+import { MapPin, User, Tv, LogIn, LogOut, Edit } from "react-feather";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../../redux/actions/authActions";
+
 const cities = [
   { city: "Jaipur" },
   { city: "Noida" },
@@ -37,29 +41,37 @@ class Header extends Component {
     console.log("city is: " + city);
   };
 
+  handleLogout = (e) => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
   render() {
     const city_list = cities.map((item, index) => {
       return <option id={index}>{item.city}</option>;
     });
+    // calling user from store
+    const { user } = this.props.auth;
+    console.log(user._id);
 
     return (
       <div>
         <Navbar className="shadow">
-          <Navbar.Collapse className="mr-lg-5 ml-lg-5">
-            <Nav.Item className="ml-lg-5 mr-lg-5">
+          <Navbar.Collapse className="mr-lg-4 ml-lg-5">
+            <Nav.Item className="ml-lg-4 mr-lg-4">
               <img src={Logo} alt="Logo" style={{ width: "180px" }} />
             </Nav.Item>
             <Link to="/" style={{ color: "#332a7c" }}>
-              <Nav.Item className="mr-lg-5">Buy</Nav.Item>
+              <Nav.Item className="mr-lg-4">Buy</Nav.Item>
             </Link>
 
             <Link to="/sell" style={{ color: "#332a7c" }}>
-              <Nav.Item className="mr-lg-5">Sell</Nav.Item>
+              <Nav.Item className="mr-lg-4">Sell</Nav.Item>
             </Link>
             <Link to="/testimonials" style={{ color: "#332a7c" }}>
-              <Nav.Item className="d-flex align-items-center mr-lg-4">
+              <Nav.Item className="d-flex align-items-center mr-lg-5">
                 Bechho Tales{" "}
-                <span className="ml-lg-1 mb-2">
+                <span className="ml-lg-2 mb-2">
                   <Tv size={20} />
                 </span>
               </Nav.Item>
@@ -72,10 +84,13 @@ class Header extends Component {
             </Nav.Item>
             {/* search location  */}
             <Form
-              className="ml-lg-5 d-flex align-items-center"
+              className="d-flex align-items-center"
               onSubmit={this.handleSubmit}
+              style={{ marginLeft: "2rem" }}
             >
-              <span className="mr-2">Select a City</span>
+              <span className="mr-2" style={{ color: "#332a7c" }}>
+                Select a City
+              </span>
               <Form.Control
                 as="select"
                 name="city"
@@ -93,24 +108,54 @@ class Header extends Component {
             </InputGroup.Append> */}
             {/* search location ends */}
           </Navbar.Collapse>
-          <Navbar.Collapse>
-            <Nav.Item className="d-flex align-items-center mr-lg-5 ml-lg-5">
-              Profile
-              <span className="ml-lg-1 mb-1 ">
-                <User size={20} color="#332a7c" />
-              </span>
-            </Nav.Item>
-            {/* <Nav.Item className="d-flex align-items-center mr-lg-5">
-              Cart
-              <span className="ml-lg-1 mb-1">
-                <img src={Cart} alt="" />
-              </span>
-            </Nav.Item> */}
-          </Navbar.Collapse>
+          {/* based on user authnetication display changes */}
+          {user._id ? (
+            <Navbar.Collapse>
+              <Nav.Item
+                className="d-flex align-items-center mr-lg-4 ml-lg-4"
+                style={{ color: "#332a7c", cursor: "pointer" }}
+              >
+                <span className="mr-lg-2">Profile</span>
+                <User size={20} />
+              </Nav.Item>
+              <Nav.Item
+                className="d-flex align-items-center mr-lg-4 ml-lg-4"
+                onClick={this.handleLogout}
+                style={{ color: "#332a7c", cursor: "pointer" }}
+              >
+                <span className="mr-lg-2">Logout</span>
+                <LogOut size={20} />
+              </Nav.Item>
+            </Navbar.Collapse>
+          ) : (
+            <Navbar.Collapse>
+              <Link to="/signup" style={{ color: "#332a7c" }}>
+                <Nav.Item className="d-flex align-items-center mr-lg-4 ml-lg-4">
+                  <span className="mr-lg-2"> Signup</span>
+                  <Edit size={20} />
+                </Nav.Item>
+              </Link>
+              <Link to="/signin" style={{ color: "#332a7c" }}>
+                <Nav.Item className="d-flex align-items-center mr-lg-4 ml-lg-4">
+                  <span className="mr-lg-2">Signin</span>
+                  <LogIn size={20} />
+                </Nav.Item>
+              </Link>
+            </Navbar.Collapse>
+          )}
         </Navbar>
       </div>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logoutUser })(Header);
